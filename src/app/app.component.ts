7364b1c20f5ef4ef31bd7238eb9952d164ca9ab6
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2023-11-04 13:27:14 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'misw-4104-parcial-1';
+}
